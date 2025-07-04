748bf7f4c48c37fbb42a1ad16720217e147df2fa
Nitish Kumar <nitish.ksharmaa@gmail.com>
2025-07-04 14:10:40 +0530

    Create IconComponent.jsx

---
+++ IconComponent.jsx
@@ -0,0 +1,11 @@
+import { FaBeer } from 'react-icons/fa';
+
+const IconComponent = () => {
+  return (
+    <div style={{fontSize: "30px", color: "gold"}}>
+        <FaBeer />
+    </div>
+  )
+}
+
+export default IconComponent
