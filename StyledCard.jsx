06e80c89078bf275bcf5224f3c091c1521e2231d
Nitish Kumar <nitish.ksharmaa@gmail.com>
2025-07-04 14:11:04 +0530

    Create StyledCard.jsx

---
+++ StyledCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const StyledCard = () => {
+  return (
+    <div style={{backgroundColor: 'lightblue', padding:'20px', borderRadius: '10px', color: 'darkblue'}}>
+        <h1>Learning REACT.JS</h1>
+        <p>I am learning react.js and I'm loving it</p>
+    </div>
+  )
+}
+
+export default StyledCard
