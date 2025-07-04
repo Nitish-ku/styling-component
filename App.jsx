3c8acd7c69d27da166a35b6658767c3bed7565a4
Nitish Kumar <nitish.ksharmaa@gmail.com>
2025-07-04 14:09:36 +0530

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import IconComponent from './components/IconComponent'
+import ProfileCard from './components/ProfileCard'
+import StyledCard from './components/StyledCard'
+
+const App = () => {
+  return (
+    <div>
+      <StyledCard/>
+      <ProfileCard/>
+      <IconComponent/>
+    </div>
+  )
+}
+
+export default App
